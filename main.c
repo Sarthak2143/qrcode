d2ee8c13211b9de4a89273aa922229118b453bbf
Sarthak Tomar <sarthaktomar2143@gmail.com>
2025-06-25 08:56:07 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+	// how do you find the minm version of qrcode needed to encode the given data?
+	// we'll be only using alphanumeric for now
+	// ./qrcode --encode "hello123" -Ec L
+	// here we are using error correction level - low
+	return 0;
+}
